0798494946574fa53b5effc68d9ec56ab262421d
BrandonPilane <BrandonPilane@gmail.com>
2025-09-23 21:10:27 +0200

    Initial commitmentation

---
+++ README.md
@@ -0,0 +1,1 @@
+I'm bored, and i wanna figure out how to deploy a svelte app.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1
+	class="text-center font-mono text-5xl font-black capitalize transition-all duration-200 ease-in-out text-shadow-none hover:tracking-wide hover:text-pink-800 hover:text-shadow-md hover:text-shadow-pink-800 dark:hover:text-pink- dark:hover:text-shadow-pink-"
+>
+	F_ck you!
+</h1>
